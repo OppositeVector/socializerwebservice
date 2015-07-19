3b357a3eb4fd32bc4916992841656c2934869734
OppositeVector <iloriginal@gmil.com>
2015-07-19 13:24:25 +0300

    CORS all allow

--- SessionController.js
+++ SessionController.js
@@ -20,6 +20,12 @@ function controller(express, mongoose, act, nonSessioned, sessioned, authenticat
 	actions = act;
 
 	app.use(function(req, res, next) {
+	  	res.header("Access-Control-Allow-Origin", "*");
+	 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	 	next();
+	});
+
+	app.use(function(req, res, next) {
 		if(req != null) {
 			if(req.headers != null) {
 				console.log(JSON.stringify(req.headers));
