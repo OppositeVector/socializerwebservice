4307b092fd5c0ef4ead8b5b3a8bf26b74e42b9c3
OppositeVector <iloriginal@gmil.com>
2015-07-19 13:29:29 +0300

    CORS try 2

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ function NonSessioned() {
 	});
 
 	app.options("/*", function (req, res) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Methods", "POST");
-		res.header("Access-Control-Allow-Headers", "Content-Type");
+		//res.header("Access-Control-Allow-Origin", "*");
+		//res.header("Access-Control-Allow-Methods", "POST");
+		//res.header("Access-Control-Allow-Headers", "Content-Type");
 		console.log(JSON.stringify(res.headers));
 		res.status(200);
 		res.send();
