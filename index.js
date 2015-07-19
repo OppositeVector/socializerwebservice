ef4b52ab57e32866433b72e43232bfc0ede621f0
OppositeVector <iloriginal@gmil.com>
2015-07-19 17:58:21 +0300

    some user tests

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function AuthenticatedSession() {
 
 	app.post("/createGroup", function (req,res) {
 		
-		console.log(req.body);
+		console.log(req.body + "\n" + req.session.user);
 		if(req.body != null) {
 			if(req.body.group != null) {
 				service.CreateGroup(req.session.user, req.body.group, res);
