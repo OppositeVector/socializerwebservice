4998998909bce24f1acdaaa60da40305fd66e164
OppositeVector <iloriginal@gmil.com>
2015-07-19 12:45:53 +0300

    changes to CORS

--- ServiceController.js
+++ ServiceController.js
@@ -19,8 +19,13 @@ module.exports = function(express, mongoose) {
 	function Actions() {
 
 		function SendJson(json, res) {
-			res.header("Access-Control-Allow-Origin","*");
-			res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+
+			res.header("Access-Control-Allow-Methods", "GET, POST");
+			res.header("Access-Control-Allow-Credentials", "true");
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "Content-Type, *");
+			// res.header("Access-Control-Allow-Origin","*");
+			// res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 			app.set("json spaces",4);
 			res.set("Content-Type","application/json");
 			res.status(200);
